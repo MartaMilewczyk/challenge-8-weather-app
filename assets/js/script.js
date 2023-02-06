f6cb2f2763e00ea767f16145886634f3900fd11f
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-02-06 17:57:59 +0100

    added date, weather parameters display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
+const dateTodayEl = $('#date-today');
+const cityDisplayEl = $('#city-display');
+const temperatureEl= $('#temperature-today');
+const windEl = $('#wind-speed-today');
+const humidityEl = $('#humidity-today');
+const forecastEl = $('#forecast');
+
+const todayDate = dayjs().format('dddd D MMM YYYY');
+const kelvinTmp = 273;
+
+dateTodayEl.append(todayDate);
 
 $('#search-button').on('click', function (event) {
     event.preventDefault();
     
     const APIkey = "5855cfc92433887fd60ef500d9af8040";
     const cityName = $('#search-input').val().trim();
-    
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`
-
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+
+        // display city from input and related weather parameters
+        cityDisplayEl.append(cityName);
+        temperatureEl.append(`${Math.floor(response.main.temp - kelvinTmp)} °C`);
+        windEl.append(`${response.wind.speed}  m/s`);
+        humidityEl.append(`${response.main.humidity} %`);
+        console.log(response);
+
     })
     
     
